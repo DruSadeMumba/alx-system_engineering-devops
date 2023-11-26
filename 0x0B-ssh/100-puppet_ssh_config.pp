578b513e8856a89d35600b463feb561cd56b21b9
Dru Mumba <drusademumba@gmail.com>
2023-11-26 21:21:13 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 # Puppet manifest for SSH client configuration
-include stdlib
 
 file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
