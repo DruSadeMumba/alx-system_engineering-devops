91d742edd35dab0c817ed5469334141397a9f5ce
Dru Mumba <drusademumba@gmail.com>
2023-12-04 15:20:03 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,8 +8,6 @@ exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  unless      => 'sudo grep -q "add_header X-Served-By" /etc/nginx/nginx.conf',
-  require     => Package['nginx'],
   before      => Exec['restart Nginx'],
 }
 
