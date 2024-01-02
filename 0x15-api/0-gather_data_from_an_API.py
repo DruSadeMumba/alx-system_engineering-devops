0607363cd7a6dbb2dda1badd77a91145e6bcb88a
Dru Mumba <drusademumba@gmail.com>
2024-01-02 13:04:18 +0300

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Gather data from an API"""
+import sys
+
+import requests
+
+
+def get_progress(emp_id):
+    """get employee progress"""
+    base = f"https://jsonplaceholder.typicode.com"
+    url = f"{base}/todos?userId={emp_id}"
+    res = requests.get(url)
+
+    if res.status_code == 200:
+        tsks = res.json()
+        comp = [task for task in tsks if task["completed"]]
+        name = requests.get(f"{base}/users/{sys.argv[1]}").json().get("name")
+
+        print(f"Employee {name} is done with tasks ({len(comp)}/{len(tsks)}):")
+        for task in comp:
+            print(f"\t{task['title']}")
+    else:
+        print("Error")
+
+
+if __name__ == "__main__":
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        sys.exit(1)
+    get_progress(employee_id)
