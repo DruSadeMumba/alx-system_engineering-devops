641c5758437943de721b305f056735d9db249e4d
Dru Mumba <drusademumba@gmail.com>
2024-01-16 13:59:12 +0300

    Write a function that queries the Reddit API and returns the number of subscribers for a given subreddit.es

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""A func that queries the Reddit API and returns the num of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    try:
+        response = requests.get(url, allow_redirects=False)
+        data = response.json()
+        if 'error' in data or response.status_code != 200:
+            return 0
+        return data['data']['subscribers']
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
